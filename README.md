99d1318880dba3b6b5628d7f1b82723838e45a5c
Eric Lee <eric yl0104@gmail.com>
2023-08-08 17:20:02 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Personal Portfolio Website
+
+This is my personal portfolio website that I have built from scratch using HTML, CSS, and JavaScript. This website includes a brief introduction about me, contact information, and a list of my projects. There is a navigation bar at the top of the website that when clicked, is scrolled to the correct tab. The website is also adjusted to the correct screen size. 
---
+++ app.js
@@ -0,0 +1,87 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+// Display Mobile Menu
+const mobileMenu = () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+  };
+  
+menu.addEventListener('click', mobileMenu);
+
+
+// function showText(imageNum){
+  // document.querySelectorAll('.project_blocks img').forEach(element =>{
+  //   document.querySelector('.popup-image').style.display = 'block';
+  //   document.querySelector('.popup-image img').src = image.getAttribute('src');
+  // });
+
+  
+  
+    // const textElements = document.querySelectorAll('.text-container p');
+    // textElements.forEach(element => {
+    //   element.classList.add('hidden');
+    // });
+    // const textElement = document.getElementById('text${imageNum}');
+    // textElement.classList.remove('hidden');
+    // console.log("hello");
+// }
+
+//Project section Pop-up Window
+// document.querySelectorAll('.project_blocks img').forEach(image =>{
+//   image.onclick = () =>{
+//     document.querySelector('.popup-image').style.display = 'block';
+//     document.querySelector('.popup-image img').src = image.getAttribute('src');
+//   }
+// });
+document.querySelector('.popup-image span').onclick = () =>{
+  document.querySelector('.popup-image').style.display = 'none';
+}
+
+
+
+// Get references to the popup elements
+const popup = document.querySelector('.popup-image');
+const popupImage = popup.querySelector('img');
+const textContainer = popup.querySelector('.text-container');
+
+// Get references to the project blocks (images)
+const projectBlocks = document.querySelectorAll('.project_blocks');
+
+// Function to show the popup with the selected image and text
+function showPopup(imageSrc, textId) {
+  popupImage.src = imageSrc;
+  
+  // Hide all text elements
+  const textElements = textContainer.querySelectorAll('p');
+  textElements.forEach(element => {
+    element.classList.add('hidden');
+  });
+  
+  // Show the selected text
+  const selectedText = document.getElementById(textId);
+  selectedText.classList.remove('hidden');
+  
+  // Show the popup
+  popup.style.display = 'block';
+}
+
+// Add click event listeners to each project block (image)
+projectBlocks.forEach((block, index) => {
+  block.addEventListener('click', () => {
+    const imageSrc = block.querySelector('.project_img').src;
+    const textId = `text${index + 1}`;
+    showPopup(imageSrc, textId);
+  });
+});
+
+// // Close the popup when the close button (x) is clicked
+// const closeButton = popup.querySelector('span');
+// closeButton.addEventListener('click', () => {
+//   popup.style.display = 'none';
+// });
+
+
+
+
+
