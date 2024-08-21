93ba329d478c914c5a0a953f392e375164c048ef
Eric-y-Lee <135090112+Eric-y-Lee@users.noreply.github.com>
2024-08-20 23:15:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -80,8 +80,34 @@ projectBlocks.forEach((block, index) => {
 // closeButton.addEventListener('click', () => {
 //   popup.style.display = 'none';
 // });
+let slideIndex = [1,1];
+let slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
 
 
+const videoContainer = document.querySelector('.project_blocks')
+const video = document.querySelector('.vid');
 
+videoContainer.addEventListener("mouseenter", () =>{
+  video.play();
+});
 
-
+videoContainer.addEventListener("mouseleave", () =>{
+  video.pause();
+});
